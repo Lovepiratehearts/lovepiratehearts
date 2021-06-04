6615f9252e6a9ce6c9b8f1725ddfddd008e5d916
Ernesto Ruiz <ruizmurcia@gmail.com>
2021-06-04 21:34:44 +0200

    Contact form with smtp.js

--- content/english/contact/_index.md
+++ content/english/contact/_index.md
@@ -1,25 +1,12 @@
 ---
 title: "Contact"
-description : "this is a meta description"
+description: "Contact page"
 
 office:
-  title : "Central Office"
-  mobile : "0124857985320"
-  email : "demo@email.com"
-  location : "Dhaka, Bangladedsh"
-  content : "Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna"
+  title: "Contact us"
+  email: "info@lovepiratehearts.com"
+  location: "United States"
+  content: "If your have any problem with your order or you just want to say hi!, you can send us an email or use the form below"
 
-# opennig hour
-opennig_hour:
-  title : "Opening Hours"
-  day_time:
-    - "Monday: 9:00 – 19:00"
-    - "Tuesday: 9:00 – 19:00"
-    - "Wednesday: 9:00 – 19:00"
-    - "Thursday: 9:00 – 19:00"
-    - "Friday: 9:00 – 19:00"
-    - "Saturday: 9:00 – 19:00"
-    - "sunday: 9:00 – 19:00"
-    
 draft: false
 ---
--- themes/vex-hugo/assets/js/script.js
+++ themes/vex-hugo/assets/js/script.js
@@ -1,51 +1,78 @@
-// Preloader js    
-$(window).on('load', function () {
-  $('.preloader').fadeOut(100);
+// Preloader js
+$(window).on("load", function () {
+  $(".preloader").fadeOut(100);
 });
 
 (function ($) {
-  'use strict';
+  "use strict";
 
-  
   // product Slider
-  $('.product-image-slider').slick({
+  $(".product-image-slider").slick({
     autoplay: false,
     infinite: true,
     arrows: false,
     dots: true,
     customPaging: function (slider, i) {
-      var image = $(slider.$slides[i]).data('image');
+      var image = $(slider.$slides[i]).data("image");
       return '<img class="img-fluid" src="' + image + '" alt="product-image">';
-    }
+    },
   });
 
   // Product slider
-  $('.product-slider').slick({
+  $(".product-slider").slick({
     infinite: true,
     slidesToShow: 4,
     slidesToScroll: 1,
     autoplay: true,
     dots: false,
     arrows: false,
-    responsive: [{
+    responsive: [
+      {
         breakpoint: 1024,
         settings: {
-          slidesToShow: 3
-        }
+          slidesToShow: 3,
+        },
       },
       {
         breakpoint: 600,
         settings: {
-          slidesToShow: 2
-        }
+          slidesToShow: 2,
+        },
       },
       {
         breakpoint: 480,
         settings: {
-          slidesToShow: 1
-        }
-      }
-    ]
+          slidesToShow: 1,
+        },
+      },
+    ],
   });
 
+  //Contact form
+  $('#alert').hide()
+
+  $("form").submit(function (event) {
+    var formData = {
+      name: $("#name").val(),
+      email: $("#email").val(),
+      message: $("#message").val()
+    };
+
+    Email.send({
+      SecureToken: "01942de1-e0ba-4b42-94d2-2ccaffa0c99b",
+      To: "lovepiratehearts+contactform@gmail.com",
+      From: formData.email,
+      Subject: "Contact Form",
+      Body: JSON.stringify(formData),
+    }).then((message) => {
+      $('#alert').fadeIn();     
+      setTimeout(function() {
+        $("#alert").fadeOut();           
+      },5000);
+    });
+
+    event.preventDefault();
+  });
+
+ 
 })(jQuery);
